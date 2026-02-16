0625c03907cee65dd3b0d89b75825a0bf09d60a8
HaQT00R <40822691+sullaspqr@users.noreply.github.com>
2026-02-16 11:21:54 +0100

    Refactor token generation logic in GenerateToken.cs

--- GenerateToken.cs
+++ GenerateToken.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
-using SimpleTokenGenerate.Models;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
@@ -9,36 +7,34 @@ namespace SimpleTokenGenerate
 {
     public class GenerateToken
     {
-        
         public string Token(string uname, int id)
         {
-           
-                var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenHandler = new JwtSecurityTokenHandler();
 
+            // Fontos: ugyanaz a kulcs legyen, mint a Program.cs-ben!
             var key = Encoding.UTF8.GetBytes("Ez egy 16 karakter hosszú szoveg legalább");
 
             var claimList = new List<Claim>
-                    {
+            {
+                new Claim(JwtRegisteredClaimNames.Name, uname),
+                new Claim(JwtRegisteredClaimNames.Sub, id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()) // Egyedi azonosító a tokennek
+            };
 
-                        new Claim(JwtRegisteredClaimNames.Name, uname),
-                         new Claim(JwtRegisteredClaimNames.Sub, id.ToString())
+            var tokenDescription = new SecurityTokenDescriptor
+            {
+                Audience = "auth-client",
+                Issuer = "auth-api",
+                Subject = new ClaimsIdentity(claimList),
+                IssuedAt = DateTime.UtcNow,        // <--- Itt adjuk hozzá az iat mezőt!
+                Expires = DateTime.UtcNow.AddDays(1), // UtcNow használata javasolt
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key), 
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
 
-                    };
-
-                var tokenDescription = new SecurityTokenDescriptor
-                {
-                    Audience = "auth-client",
-                    Issuer = "auth-api",
-                    Subject = new ClaimsIdentity(claimList),
-                    Expires = DateTime.Now.AddDays(1),
-                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-                };
-
-                var token = tokenHandler.CreateToken(tokenDescription);
-
-                return tokenHandler.WriteToken(token);
-
-            }
-
+            var token = tokenHandler.CreateToken(tokenDescription);
+            return tokenHandler.WriteToken(token);
         }
     }
+}
